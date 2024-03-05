328b3d4fa69d9f1a102d246e2699dad3b8e1dd34
Sagar Makol <sagar.makol.2001@gmail.com>
2024-03-05 15:50:47 +0530

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	new file:   frontend/public/images/airpods.jpg 	new file:   frontend/public/images/alexa.jpg 	new file:   frontend/public/images/camera.jpg 	new file:   frontend/public/images/mouse.jpg 	new file:   frontend/public/images/phone.jpg 	new file:   frontend/public/images/playstation.jpg 	new file:   frontend/public/images/sample.jpg 	modified:   frontend/src/App.js 	new file:   frontend/src/assets/logo.png 	new file:   frontend/src/assets/styles/bootstrap.custom.css 	new file:   frontend/src/assets/styles/index.css 	new file:   frontend/src/components/Footer.jsx 	deleted:    frontend/src/components/Header.js 	new file:   frontend/src/components/Header.jsx 	new file:   frontend/src/components/Product.jsx 	deleted:    frontend/src/index.css 	modified:   frontend/src/index.js 	new file:   frontend/src/products.js 	new file:   frontend/src/screens/HomeScreen.jsx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
@@ -8,9 +10,10 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Outlet />
         </Container>
       </main>
+      <Footer />
     </>
   );
 };
